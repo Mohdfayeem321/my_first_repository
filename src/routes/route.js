7daa0dc1028d4ca88898070d4c5dc55e1f8b2647
Mohdfayeem321 <106511844+Mohdfayeem321@users.noreply.github.com>
2022-08-09 13:05:31 +0530

    routeupdate

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const logger = require('../logger/logger.js');
-const helper = require('../util/helper.js');
-const formatter = require('../validator/formatter.js');
+const lodash = require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     
     res.send('Welcome to my application. I am mohd fayeem and a part of FunctionUp Plutonium cohort.')
+    let months = ["jan", "feb", "march", "april", "may", "june", "july", "aug", "sep", "oct", "nov", "dec"]
+    let result = lodash.chunk(months, 4);
+    console.log(result)
+    let oddNum = [1,3,5,7,9,11,13,15,17,19]
+    let result2 = lodash.tail(oddNum);
+    console.log(result2)
+    console.log(lodash.union([2,3], [1,2], [8,9], [10,12], [10,11]));
 
-    logger.welcome()
-    helper.date()
-    helper.month()
-    helper.batchInfo()
-    formatter.trim()
-    formatter.lower()
-    formatter.upper()
+    let obj = ([["Horror", "The shining"], ["Drama", "Titanic"], ["Thriller", "Shuttler Island"], ["Fantasy", "Pans labyrinth"]]);
+    console.log(lodash.fromPairs(obj))
+
 
 });
 
@@ -23,4 +24,3 @@ router.get('/give-me-students-data',function(req, res){
 
 })
 module.exports = router;
-// adding this comment for no reason
